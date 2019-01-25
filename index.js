46bfa77182fcc41429cb6e0a0e5d6e29d8772a8f
Nadia27 <jones.nadia.l@gmail.com>
2019-01-24 22:37:25 -0500

    added sounds folder with sound files

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function handleClick () {
-  alert("I was clicked");
-}
-
+// Store NodeList in array
 var drums = document.getElementsByClassName("drum");
 
+// If it contains items
 if (drums.length > 0) {
+  // Loop through each item and addEventListener
   for (var i = 0; i < drums.length; i++) {
     drums[i].addEventListener("click", handleClick);
   }
+}
+
+function handleClick () {
+  alert("I was clicked");
 }
