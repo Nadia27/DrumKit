65fd704b480829b0ec5ec42afb440a5485561a46
Nadia27 <jones.nadia.l@gmail.com>
2019-01-25 13:03:50 -0500

    fine tuned button animations with timeout function...app complete

--- index.js
+++ index.js
@@ -69,5 +69,9 @@ function soundPlayback(key) {
 function buttonAnimation (currentKey) {
   var activeButton = document.querySelector("." + currentKey);
 
-  activeButton.classList.add("pressed"); 
+  activeButton.classList.add("pressed");
+
+setTimeout(function () {
+  activeButton.classList.remove("pressed");
+}, 100);
 }
